638113247338d6f670857eb717a7c2179a6e2ee4
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-07-21 19:31:38 +0200

    Support for ACR with all available tiers

--- arm-estimator/Products/ContainerRegistry/ContainerRegistryEstimationCalculation.cs
+++ arm-estimator/Products/ContainerRegistry/ContainerRegistryEstimationCalculation.cs
@@ -22,10 +22,22 @@
         // If metrics contain "Basic Registry Unit" metric, we need to calculate a full
         // month estimation as they are presented as single-day metric
         var basicRegistryUnitMetric = items.SingleOrDefault(_ => _.meterId == "5c9e7a65-5784-494c-9718-7749d4075dd9");
+        var standardRegistryUnitMetric = items.SingleOrDefault(_ => _.meterId == "2f8e1105-47d0-4105-a934-b2175c766a71");
+        var premiumRegistryUnitMetric = items.SingleOrDefault(_ => _.meterId == "984c39d8-2499-4233-8a97-fb002a809f8f");
         if (basicRegistryUnitMetric != null)
         {
             estimatedCost = items.Where(_ => _.meterId != "5c9e7a65-5784-494c-9718-7749d4075dd9").Select(_ => _.retailPrice).Sum();
             estimatedCost += basicRegistryUnitMetric.retailPrice * 30;
+        }
+        else if (standardRegistryUnitMetric != null)
+        {
+            estimatedCost = items.Where(_ => _.meterId != "2f8e1105-47d0-4105-a934-b2175c766a71").Select(_ => _.retailPrice).Sum();
+            estimatedCost += standardRegistryUnitMetric.retailPrice * 30;
+        }
+        else if (premiumRegistryUnitMetric != null)
+        {
+            estimatedCost = items.Where(_ => _.meterId != "984c39d8-2499-4233-8a97-fb002a809f8f").Select(_ => _.retailPrice).Sum();
+            estimatedCost += premiumRegistryUnitMetric.retailPrice * 30;
         }
         else
         {
--- arm-estimator/templates/acr.bicep
+++ arm-estimator/templates/acr.bicep
@@ -5,3 +5,19 @@ resource acr 'Microsoft.ContainerRegistry/registries@2021-09-01' = {
     name: 'Basic'
   }
 }
+
+resource acr2 'Microsoft.ContainerRegistry/registries@2021-09-01' = {
+  name: 'armestimatoracr2'
+  location: 'westeurope'
+  sku: {
+    name: 'Standard'
+  }
+}
+
+resource acr3 'Microsoft.ContainerRegistry/registries@2021-09-01' = {
+  name: 'armestimatoracr3'
+  location: 'westeurope'
+  sku: {
+    name: 'Premium'
+  }
+}
