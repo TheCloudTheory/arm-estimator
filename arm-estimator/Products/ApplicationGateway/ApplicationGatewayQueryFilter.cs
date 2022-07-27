f896f42f904bb1425d1208a952c3907d7519556f
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-07-27 23:05:26 +0200

    Added support for Application Gateway

---
+++ arm-estimator/Products/ApplicationGateway/ApplicationGatewayQueryFilter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+internal class ApplicationGatewayQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH319QDM5ZN";
+
+    private readonly WhatIfAfterBeforeChange afterState;
+    private readonly ILogger logger;
+
+    public ApplicationGatewayQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+        this.afterState = afterState;
+        this.logger = logger;
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        if(this.afterState.properties == null)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when properties are not unavailable.");
+            return null;
+        }
+
+        if (this.afterState.properties.TryGetValue("sku", out var skuData) == false)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when SKU is unavailable.");
+            return null;
+        }
+
+        if (skuData == null)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when SKU is unavailable.");
+            return null;
+        }
+
+        var sku = ((JsonElement)skuData).Deserialize<Dictionary<string, object>>();
+        if (sku == null)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when SKU is invalid.");
+            return null;
+        }
+
+        if(sku.ContainsKey("name") == false)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when SKU is invalid.");
+            return null;
+        }
+
+        var skuName = sku["name"]?.ToString();
+        if(skuName == null)
+        {
+            this.logger.LogError("Can't create a filter for Application Gateway when SKU is invalid.");
+            return null;
+        }
+
+        var skuIds = ApplicationGatewaySupportedData.SkuToSkuIdMap[skuName];
+        var skuIdsFilter = string.Join(" or ", skuIds.Select(_ => $"skuId eq '{_}'"));
+
+        return $"$filter=serviceId eq '{ServiceId}' and armRegionName eq '{location}' and ({skuIdsFilter})";
+    }
+}
---
+++ arm-estimator/Products/ApplicationGateway/ApplicationGatewaySupportedData.cs
@@ -0,0 +1,48 @@
+﻿internal class ApplicationGatewaySupportedData
+{
+    public static readonly IReadOnlyDictionary<string, string[]> SkuToSkuIdMap = new Dictionary<string, string[]>()
+    {
+        {
+            "Standard_Small", new[] 
+            {
+                "DZH318Z0BNXC/0037"
+            }
+        },
+        {
+            "Standard_Medium", new[]
+            {
+                "DZH318Z0BNXC/002R"
+            }
+        },
+        {
+            "Standard_Large", new[]
+            {
+                "DZH318Z0BNXC/000K"
+            }
+        },
+        {
+            "Standard_v2", new[]
+            {
+                "DZH318Z0BSD1/0004"
+            }
+        },
+        {
+            "WAF_Medium", new[]
+            {
+                "DZH318Z0BNVT/000V"
+            }
+        },
+        {
+            "WAF_Large", new[]
+            {
+                "DZH318Z0BNVT/0007"
+            }
+        },
+        {
+            "WAF_v2", new[]
+            {
+                "DZH318Z0BSD0/000B"
+            }
+        }
+    };
+}
