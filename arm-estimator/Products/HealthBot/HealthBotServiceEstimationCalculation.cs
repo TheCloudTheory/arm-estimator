a2ee0f97a26898a9f03a4cfc97a482e734c3c473
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-07-29 10:06:23 +0200

    Added support for Health Bot

---
+++ arm-estimator/Products/HealthBot/HealthBotServiceEstimationCalculation.cs
@@ -0,0 +1,35 @@
+﻿internal class HealthBotServiceEstimationCalculation : BaseEstimation, IEstimationCalculation
+{
+    public HealthBotServiceEstimationCalculation(RetailItem[] items, WhatIfAfterBeforeChange change)
+        : base(items, change)
+    {
+    }
+
+    public IOrderedEnumerable<RetailItem> GetItems()
+    {
+        var consumptionMetrics = this.items.Where(_ => _.type != "Reservation");
+
+        return this.items.Where(_ => _.type != "Reservation").OrderByDescending(_ => _.retailPrice);
+    }
+
+    public double GetTotalCost()
+    {
+        double? estimatedCost = 0;
+        var items = GetItems();
+
+        foreach (var item in items)
+        {
+            // Standard Unit
+            if (item.meterId == "1faf3518-3945-58e9-8782-80f95cd3903a")
+            {
+                estimatedCost += item.retailPrice * 30;
+            }
+            else
+            {
+                estimatedCost += item.retailPrice;
+            }
+        }
+
+        return estimatedCost == null ? 0 : (double)estimatedCost;
+    }
+}
---
+++ arm-estimator/Products/HealthBot/HealthBotServiceQueryFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+internal class HealthBotServiceQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH318J9W0D1";
+
+    private readonly WhatIfAfterBeforeChange afterState;
+    private readonly ILogger logger;
+
+    public HealthBotServiceQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+        this.afterState = afterState;
+        this.logger = logger;
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        var sku = this.afterState.sku?.name;
+        if (sku == null)
+        {
+            this.logger.LogError("Can't create a filter for Bot Service when SKU is unavailable.");
+            return null;
+        }
+
+        var skuIds = HealthBotServiceSupportedData.SkuToSkuIdMap[sku];
+        var skuIdsFilter = string.Join(" or ", skuIds.Select(_ => $"skuId eq '{_}'"));
+
+        return $"$filter=serviceId eq '{ServiceId}' and armRegionName eq '{location}' and ({skuIdsFilter})";
+    }
+}
