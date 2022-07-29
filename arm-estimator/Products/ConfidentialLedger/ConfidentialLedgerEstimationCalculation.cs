24c809d05330419799d191b82b7c56fcbf158857
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-07-29 13:49:42 +0200

    Added support for Confidential Ledger

---
+++ arm-estimator/Products/ConfidentialLedger/ConfidentialLedgerEstimationCalculation.cs
@@ -0,0 +1,35 @@
+﻿internal class ConfidentialLedgerEstimationCalculation : BaseEstimation, IEstimationCalculation
+{
+    public ConfidentialLedgerEstimationCalculation(RetailItem[] items, WhatIfAfterBeforeChange change)
+        : base(items, change)
+    {
+    }
+
+    public IOrderedEnumerable<RetailItem> GetItems()
+    {
+        var consumptionMetrics = this.items.Where(_ => _.type != "Reservation");
+
+        return this.items.Where(_ => _.type != "Reservation").OrderByDescending(_ => _.retailPrice);
+    }
+
+    public double GetTotalCost()
+    {
+        double? estimatedCost = 0;
+        var items = GetItems();
+
+        foreach (var item in items)
+        {
+            // Ledger P1 Instance
+            if (item.meterId == "db454beb-e666-5669-94bc-5683d7e5b602")
+            {
+                estimatedCost += item.retailPrice * HoursInMonth;
+            }
+            else
+            {
+                estimatedCost += item.retailPrice;
+            }
+        }
+
+        return estimatedCost == null ? 0 : (double)estimatedCost;
+    }
+}
---
+++ arm-estimator/templates/bicep/confidentialledger.bicep
@@ -0,0 +1,7 @@
+resource ledger 'Microsoft.ConfidentialLedger/ledgers@2022-05-13' = {
+  name: 'ledger'
+  location: 'westeurope'
+  properties: {
+   ledgerType: 'Public' 
+  }
+}
