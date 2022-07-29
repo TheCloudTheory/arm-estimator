610a515000e726aeb697caa5603c9f046d9bdbfc
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-07-29 09:39:23 +0200

    Added support for Bastion

---
+++ arm-estimator/Products/Bastion/BastionEstimationCalculation.cs
@@ -0,0 +1,55 @@
+﻿internal class BastionEstimationCalculation : BaseEstimation, IEstimationCalculation
+{
+    public BastionEstimationCalculation(RetailItem[] items, WhatIfAfterBeforeChange change)
+        : base(items, change)
+    {
+    }
+
+    public IOrderedEnumerable<RetailItem> GetItems()
+    {
+        var consumptionMetrics = this.items.Where(_ => _.type != "Reservation");
+
+        return this.items.Where(_ => _.type != "Reservation").OrderByDescending(_ => _.retailPrice);
+    }
+
+    public double GetTotalCost()
+    {
+        double? estimatedCost = 0;
+        var items = GetItems();
+
+        var scaleUnits = 0;
+        if(this.change.properties != null)
+        {
+            var scaleUnitsData = this.change.properties["scaleUnits"]?.ToString();
+            if(scaleUnitsData != null)
+            {
+                scaleUnits = int.Parse(scaleUnitsData);
+            }
+        }
+
+        foreach (var item in items)
+        {
+            // Basic Gateway
+            if (item.meterId == "c6d29001-2ec1-43ce-8ec8-8c753399ff6c")
+            {
+                estimatedCost += item.retailPrice * HoursInMonth;
+            }
+            // Standard Gateway
+            else if (item.meterId == "4836590f-078a-5184-b23a-63ad1c7979cc")
+            {
+                estimatedCost += item.retailPrice * HoursInMonth;
+            }
+            // Standard Additional Gateway
+            else if (item.meterId == "bd08452f-cf67-539c-baa1-1ef7d6fb13d0")
+            {
+                estimatedCost += item.retailPrice * HoursInMonth * scaleUnits;
+            }
+            else
+            {
+                estimatedCost += item.retailPrice;
+            }
+        }
+
+        return estimatedCost == null ? 0 : (double)estimatedCost;
+    }
+}
---
+++ arm-estimator/Products/Bastion/BastionQueryFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+internal class BastionQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH3160859B7";
+
+    private readonly WhatIfAfterBeforeChange afterState;
+    private readonly ILogger logger;
+
+    public BastionQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+        this.afterState = afterState;
+        this.logger = logger;
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        var sku = this.afterState.sku?.name;
+        if (sku == null)
+        {
+            this.logger.LogError("Can't create a filter for Azure Bastion when SKU is unavailable.");
+            return null;
+        }
+
+        var skuIds = BastionSupportedData.SkuToSkuIdMap[sku];
+        var skuIdsFilter = string.Join(" or ", skuIds.Select(_ => $"skuId eq '{_}'"));
+
+        return $"$filter=serviceId eq '{ServiceId}' and armRegionName eq '{location}' and ({skuIdsFilter})";
+    }
+}
