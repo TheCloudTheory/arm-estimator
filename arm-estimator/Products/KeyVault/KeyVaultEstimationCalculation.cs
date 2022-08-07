552994b8c1a091bce7c720fdc3aaa34a21fe1e3d
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-08-07 21:02:14 +0200

    Added support for Key Vault

---
+++ arm-estimator/Products/KeyVault/KeyVaultEstimationCalculation.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Core;
+
+internal class KeyVaultEstimationCalculation : BaseEstimation, IEstimationCalculation
+{
+    public KeyVaultEstimationCalculation(RetailItem[] items, ResourceIdentifier id, WhatIfAfterBeforeChange change)
+        : base(items, id, change)
+    {
+    }
+
+    public IOrderedEnumerable<RetailItem> GetItems()
+    {
+        return this.items.OrderByDescending(_ => _.retailPrice);
+    }
+
+    public double GetTotalCost(WhatIfChange[] changes)
+    {
+        double? estimatedCost = 0;
+        var items = GetItems();
+
+        foreach (var item in items)
+        {
+            // Standard B1 Instance
+            if (item.meterId == "d4a3d8b9-a5c1-5a86-8706-3e26a9b12e5a")
+            {
+                estimatedCost += item.retailPrice * HoursInMonth;
+            }
+            else
+            {
+                estimatedCost += item.retailPrice;
+            }
+        }
+
+        return estimatedCost == null ? 0 : (double)estimatedCost;
+    }
+}
---
+++ arm-estimator/Products/KeyVault/KeyVaultQueryFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+internal class KeyVaultQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH3157JCZ2M";
+
+    private readonly WhatIfAfterBeforeChange afterState;
+    private readonly ILogger logger;
+
+    public KeyVaultQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+        this.afterState = afterState;
+        this.logger = logger;
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        var sku = this.afterState.sku?.name;
+        if(sku == null)
+        {
+            if(this.afterState.properties != null && this.afterState.properties.ContainsKey("sku"))
+            {
+                var skuData = ((JsonElement)this.afterState.properties["sku"]).Deserialize<Dictionary<string, string>>();
+                if(skuData != null && skuData.ContainsKey("name"))
+                {
+                    sku = skuData["name"];
+                }
+            }
+        }
+
+        if (sku == null)
+        {
+            this.logger.LogError("Can't create a filter for Key Vault when SKU is unavailable.");
+            return null;
+        }
+
+        var skuIds = KeyVaultSupportedData.SkuToSkuIdMap[sku];
+        var skuIdsFilter = string.Join(" or ", skuIds.Select(_ => $"skuId eq '{_}'"));
+
+        return $"serviceId eq '{ServiceId}' and armRegionName eq '{location}' and ({skuIdsFilter})";
+    }
+}
