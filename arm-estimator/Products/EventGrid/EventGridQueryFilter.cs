840d9520abcef798bf40161c7b9a7064755f3b27
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-08-12 10:53:11 +0200

    Added support for Event Grid

---
+++ arm-estimator/Products/EventGrid/EventGridQueryFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+internal class EventGridQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH315NX4WLD";
+
+    public EventGridQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        var sku = "Standard";
+        var skuIds = EventGridSupportedData.SkuToSkuIdMap[sku];
+        var skuIdsFilter = string.Join(" or ", skuIds.Select(_ => $"skuId eq '{_}'"));
+
+        return $"serviceId eq '{ServiceId}' and armRegionName eq '{location}' and ({skuIdsFilter})";
+    }
+}
---
+++ arm-estimator/Products/EventGrid/EventGridSupportedData.cs
@@ -0,0 +1,12 @@
+﻿internal class EventGridSupportedData
+{
+    public static readonly IReadOnlyDictionary<string, string[]> SkuToSkuIdMap = new Dictionary<string, string[]>()
+    {
+        {
+            "Standard", new[]
+            {
+                "DZH318Z0BQG3/000H"
+            }
+        }
+    };
+}
