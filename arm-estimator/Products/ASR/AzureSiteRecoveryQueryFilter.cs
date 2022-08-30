d5cd393282509f9e552e45608ad6a4217d2cc60a
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-08-30 18:48:45 +0200

    Added support for ASR

---
+++ arm-estimator/Products/ASR/AzureSiteRecoveryQueryFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+internal class AzureSiteRecoveryQueryFilter : IQueryFilter
+{
+    private const string ServiceId = "DZH3174MS2N2";
+
+    private readonly WhatIfAfterBeforeChange afterState;
+    private readonly ILogger logger;
+
+    public AzureSiteRecoveryQueryFilter(WhatIfAfterBeforeChange afterState, ILogger logger)
+    {
+        this.afterState = afterState;
+        this.logger = logger;
+    }
+
+    public string? GetFiltersBasedOnDesiredState(string location)
+    {
+        return $"serviceId eq '{ServiceId}' and armRegionName eq '{location}' and skuName eq 'Azure'";
+    }
+}
