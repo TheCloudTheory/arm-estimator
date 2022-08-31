41680f57c42b2747ed92f496ba0ec3edf0d1b647
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-08-31 20:04:58 +0200

    Added simple test for ACR

--- arm-estimator-tests/AzureContainerRegistry_ARM_Tests.cs
+++ arm-estimator-tests/AzureContainerRegistry_ARM_Tests.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace arm_estimator_tests
 {
     public class AzureContainerRegistry_ARM_Tests
@@ -8,9 +10,29 @@ namespace arm_estimator_tests
         }
 
         [Test]
-        public async Task Test1()
+        public async Task AzureContainerRegistryEstimation_ShouldBeCalculatedCorrectly()
         {
-            var ace = await Program.Main(new[] { "templates/acr.json", "f81e70a7-e819-49b2-a980-8e9c433743dd", "arm-estimator-rg", "--generateJsonOutput" });
+            var outputFilename = $"ace_test_{DateTime.Now.Ticks}";
+            var exitCode = await Program.Main(new[] { 
+                "templates/acr.json", 
+                "f81e70a7-e819-49b2-a980-8e9c433743dd", 
+                "arm-estimator-rg", 
+                "--generateJsonOutput",
+                "--jsonOutputFilename",
+                outputFilename
+            });
+
+            Assert.That(exitCode, Is.EqualTo(0));
+
+            var outputFile = File.ReadAllText($"{outputFilename}.json");
+            var output = JsonSerializer.Deserialize<EstimationOutput>(outputFile, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            Assert.That(output, Is.Not.Null);
+            Assert.That(output.TotalCost, Is.EqualTo(2.8001));
+            Assert.That(output.Delta, Is.EqualTo(2.8001));
         }
     }
 }
---
+++ arm-estimator-tests/EstimationOutput.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace arm_estimator_tests
+{
+    public class EstimationOutput
+    {
+        public double TotalCost { get; set; }
+        public double Delta { get; set; }
+        public IEnumerable<EstimatedResourceData> Resources { get; set; }
+        public string Currency { get; set; }
+
+        [JsonConstructor]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public EstimationOutput() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    }
+
+    public class EstimatedResourceData
+    {
+        public string Id { get; set; }
+        public double TotalCost { get; set; }
+        public double Delta { get; set; }
+
+        [JsonConstructor]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public EstimatedResourceData() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    }
+}
