caeeb64ffa8173ca4a44ad868ca1828278bbcd4a
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-09-01 21:10:09 +0200

    Changed the way how tests are executed

--- arm-estimator-tests/Generic_ARM_Tests.cs
+++ arm-estimator-tests/Generic_ARM_Tests.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 
 namespace arm_estimator_tests
 {
-    public class AzureContainerRegistry_ARM_Tests
+    public class Generic_ARM_Tests
     {
         [SetUp]
         public void Setup()
@@ -10,12 +10,15 @@ namespace arm_estimator_tests
         }
 
         [Test]
-        public async Task AzureContainerRegistryEstimation_ShouldBeCalculatedCorrectly()
+        [TestCase("templates/acr.json", 2.8001, 2.8001)]
+        [TestCase("templates/alert.json", 2.8001, 2.8001)]
+        [TestCase("templates/analysisservices.json", 14389.759999999998, 14389.759999999998)]
+        public async Task ResourceEstimation_ShouldBeCalculatedCorrectly(string templatePath, double totalValue, double deltaValue)
         {
             var outputFilename = $"ace_test_{DateTime.Now.Ticks}";
-            var exitCode = await Program.Main(new[] { 
-                "templates/acr.json", 
-                "f81e70a7-e819-49b2-a980-8e9c433743dd", 
+            var exitCode = await Program.Main(new[] {
+                templatePath, 
+                "f81e70a7-e819-49b2-a980-8e9c433743dd",
                 "arm-estimator-rg", 
                 "--generateJsonOutput",
                 "--jsonOutputFilename",
@@ -31,8 +34,8 @@ namespace arm_estimator_tests
             });
 
             Assert.That(output, Is.Not.Null);
-            Assert.That(output.TotalCost, Is.EqualTo(2.8001));
-            Assert.That(output.Delta, Is.EqualTo(2.8001));
+            Assert.That(output.TotalCost, Is.EqualTo(totalValue));
+            Assert.That(output.Delta, Is.EqualTo(deltaValue));
         }
     }
 }
