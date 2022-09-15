e7fbf33cc6602cf9254a2994310b8b9af02cb586
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-09-15 19:27:31 +0200

    Added support for new VM families

--- arm-estimator/Products/VirtualMachine/VirtualMachineQueryFilter.cs
+++ arm-estimator/Products/VirtualMachine/VirtualMachineQueryFilter.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text.Json;
 
 internal class VirtualMachineQueryFilter : IQueryFilter
@@ -231,6 +233,194 @@ internal class VirtualMachineQueryFilter : IQueryFilter
                 case "D64 v4":
                     familySku = "Dv4";
                     skuName = $"D64 v4";
+                    break;
+                case "D2a v4":
+                    familySku = "Dav4";
+                    skuName = $"D2a v4/D2as v4";
+                    break;
+                case "D4a v4":
+                    familySku = "Dav4";
+                    skuName = $"D4a v4/D4as v4";
+                    break;
+                case "D8a v4":
+                    familySku = "Dav4";
+                    skuName = $"D8a v4/D8as v4";
+                    break;
+                case "D16a v4":
+                    familySku = "Dav4";
+                    skuName = $"D16a v4/D16as v4";
+                    break;
+                case "D32a v4":
+                    familySku = "Dav4";
+                    skuName = $"D32a v4/D32as v4";
+                    break;
+                case "D48a v4":
+                    familySku = "Dav4";
+                    skuName = $"D48a v4/D48as v4";
+                    break;
+                case "D64a v4":
+                    familySku = "Dav4";
+                    skuName = $"D64a v4/D64as v4";
+                    break;
+                case "D96a v4":
+                    familySku = "Dav4";
+                    skuName = $"D96a v4/D96as v4";
+                    break;
+                case "D2as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D2as v4";
+                    break;
+                case "D4as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D4as v4";
+                    break;
+                case "D8as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D8as v4";
+                    break;
+                case "D16as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D16as v4";
+                    break;
+                case "D32as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D32as v4";
+                    break;
+                case "D48as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D48as v4";
+                    break;
+                case "D64as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D64as v4";
+                    break;
+                case "D96as v4":
+                    familySku = "Dasv4";
+                    skuName = $"D96as v4";
+                    break;
+                case "D2d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D2d v4";
+                    break;
+                case "D4d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D4d v4";
+                    break;
+                case "D8d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D8d v4";
+                    break;
+                case "D16d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D16d v4";
+                    break;
+                case "D32d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D32d v4";
+                    break;
+                case "D64d v4":
+                    familySku = "Ddv4";
+                    skuName = $"D64d v4";
+                    break;
+                case "D2ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D2ds v4";
+                    break;
+                case "D4ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D4ds v4";
+                    break;
+                case "D8ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D8ds v4";
+                    break;
+                case "D16ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D16ds v4";
+                    break;
+                case "D32ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D32ds v4";
+                    break;
+                case "D48ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D48ds v4";
+                    break;
+                case "D64ds v4":
+                    familySku = "Ddsv4";
+                    skuName = $"D64ds v4";
+                    break;
+                case "D2s v3":
+                    familySku = "DSv3";
+                    skuName = $"D2s v3";
+                    break;
+                case "D4s v3":
+                    familySku = "DSv3";
+                    skuName = $"D4s v3";
+                    break;
+                case "D8s v3":
+                    familySku = "DSv3";
+                    skuName = $"D8s v3";
+                    break;
+                case "D16s v3":
+                    familySku = "DSv3";
+                    skuName = $"D16s v3";
+                    break;
+                case "D32s v3":
+                    familySku = "DSv3";
+                    skuName = $"D32s v3";
+                    break;
+                case "D32-8s v3":
+                    familySku = "DSv3";
+                    skuName = $"D32-8s v3";
+                    break;
+                case "D32-16s v3":
+                    familySku = "DSv3";
+                    skuName = $"D32-16s v3";
+                    break;
+                case "D48s v3":
+                    familySku = "DSv3";
+                    skuName = $"D48s v3";
+                    break;
+                case "D64s v3":
+                    familySku = "DSv3";
+                    skuName = $"D64s v3";
+                    break;
+                case "D64-16s v3":
+                    familySku = "DSv3";
+                    skuName = $"D64-16s v3";
+                    break;
+                case "D64-32s v3":
+                    familySku = "DSv3";
+                    skuName = $"D64-32s v3";
+                    break;
+                case "D2s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D2s v4";
+                    break;
+                case "D4s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D4s v4";
+                    break;
+                case "D8s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D8s v4";
+                    break;
+                case "D16s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D16s v4";
+                    break;
+                case "D32s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D32s v4";
+                    break;
+                case "D48s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D48s v4";
+                    break;
+                case "D64s v4":
+                    familySku = "Dsv4";
+                    skuName = $"D64s v4";
                     break;
                 default:
                     this.logger.LogWarning("VM size {size} is not supported.", vmSize);
