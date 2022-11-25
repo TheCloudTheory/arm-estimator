bf3172e4a64a4f9ce6b1e5099440a4ec47dcc5fd
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-11-25 18:51:30 +0100

    Fixed AppGW estimation due to invalid handling of metrics

--- arm-estimator-tests/Generic_ARM_Tests.cs
+++ arm-estimator-tests/Generic_ARM_Tests.cs
@@ -23,6 +23,7 @@ namespace arm_estimator_tests
         [TestCase("templates/postgresql.json", 10190.492299999996d, 10190.492299999996d)]
         [TestCase("templates/servicebus.json", 688.50693000000001d, 688.50693000000001d)]
         [TestCase("templates/datafactory.json", 189.13086000000001d, 189.13086000000001d)]
+        [TestCase("templates/appgw.json", 1598.7849000000001d, 1598.7849000000001d)]
         public async Task ResourceEstimation_ShouldBeCalculatedCorrectly(string templatePath, double totalValue, double deltaValue)
         {
             var outputFilename = $"ace_test_{DateTime.Now.Ticks}";
