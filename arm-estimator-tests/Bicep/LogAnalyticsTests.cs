4e0e9e18abba5b8830f063c7c47530b2d2badc00
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-12-28 13:45:29 +0100

    Free 5GBs for Log Analytics included

--- arm-estimator-tests/Bicep/LogAnalyticsTests.cs
+++ arm-estimator-tests/Bicep/LogAnalyticsTests.cs
@@ -27,7 +27,7 @@ namespace arm_estimator_tests.Bicep
             });
 
             Assert.That(output, Is.Not.Null);
-            Assert.That(output.TotalCost, Is.EqualTo(457.64999999999998d));
+            Assert.That(output.TotalCost, Is.EqualTo(442.69999999999999d));
             Assert.That(output.TotalResourceCount, Is.EqualTo(2));
         }
     }
