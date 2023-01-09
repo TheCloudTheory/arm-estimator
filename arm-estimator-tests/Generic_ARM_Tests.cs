339844077e12c9f0c17161d34b91c1c0e16a822c
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-01-09 15:32:13 +0100

    Fixed test case

--- arm-estimator-tests/Generic_ARM_Tests.cs
+++ arm-estimator-tests/Generic_ARM_Tests.cs
@@ -75,7 +75,7 @@ namespace arm_estimator_tests
             });
 
             Assert.That(output, Is.Not.Null);
-            Assert.That(output.TotalCost, Is.EqualTo(1123.3200999999997d));
+            Assert.That(output.TotalCost, Is.EqualTo(1123.3199999999997d));
         }
     }
 }
