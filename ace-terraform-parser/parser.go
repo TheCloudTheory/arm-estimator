b9ddd9a62838bea391d275f947f916546ac4d31f
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-02-09 17:04:25 +0100

    feat(terraform): Printing errors so runners won't crash

--- ace-terraform-parser/parser.go
+++ ace-terraform-parser/parser.go
@@ -15,17 +15,29 @@ import (
 //export GenerateParsedPlan
 func GenerateParsedPlan(workingDir string, planFile string) {
 	path, err := exec.LookPath("terraform")
+	if err != nil {
+		log.Printf("error looking for Terraform executable: %s", err)
+		return
+	}
+
 	tf, err := tfexec.NewTerraform(workingDir, path)
 	if err != nil {
-		log.Fatalf("error running NewTerraform: %s", err)
+		log.Printf("error running NewTerraform: %s", err)
+		return
 	}
 
 	plan, err := tf.ShowPlanFile(context.Background(), planFile)
 	if err != nil {
-		log.Fatalf("error running Show: %s", err)
+		log.Printf("error running Show: %s", err)
+		return
 	}
 
 	ret, err := json.Marshal(plan)
+	if err != nil {
+		log.Printf("error marshalling json: %s", err)
+		return
+	}
+
 	os.WriteFile("ace-terraform-parsed-plan.json", ret, 0644)
 }
 
