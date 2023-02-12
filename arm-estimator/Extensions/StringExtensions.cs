820c6278c425d23f9aba7e2f20ad5bb9b6f29f03
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-02-12 20:50:28 +0100

    feat(output): Added extension for handling null string

---
+++ arm-estimator/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ACE.Extensions
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Returns "N/A" if tested value is null. Used mostly in places where nullability check
+        /// is not necessary (like output), while returning null value might be misleading or 
+        /// difficult to interpret.
+        /// </summary>
+        public static string GetValueOrNotAvailable(this string? nullableValue)
+        {
+            return nullableValue ?? "N/A";
+        }
+    }
+}
