6b243cbbcb77c615830c4134a58fc683e4215291
Kamil Mrzygłód <kamil@thecloudtheory.com>
2024-01-04 07:31:46 +0100

    test(keyvault): Added test for #232

---
+++ ace-tests/Constants.cs
@@ -0,0 +1,7 @@
+namespace ACE.Tests;
+
+internal class Constants
+{
+    internal const string AZURE_SUBSCRIPTION_ID = "cf70b558-b930-45e4-9048-ebcefb926adf";
+    internal const string AZURE_RESOURCE_GROUP = "arm-estimator-tests-rg";
+}
