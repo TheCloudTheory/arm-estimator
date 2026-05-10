82fe6365414eaa08855c6e2966345686e411aabe
Kamil Mrzygłód <kamil@thecloudtheory.com>
2026-05-10 08:49:51 +0200

    fix(tf): Replace data block with resource block for azurerm_resource_group and update automation account properties

--- ace-tests/templates/reworked/automation-account/tf/generic/main.tf
+++ ace-tests/templates/reworked/automation-account/tf/generic/main.tf
@@ -13,18 +13,15 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-locals {
-  name = "automation${formatdate("yyyyMMddhhmmss", timestamp())}"
-}
-
-data "azurerm_resource_group" "rg" {
-  name = "arm-estimator-tests-rg"
+resource "azurerm_resource_group" "rg" {
+  name     = "arm-estimator-tests-rg"
+  location = "West Europe"
 }
 
 resource "azurerm_automation_account" "automation" {
-  name                = local.name
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "example-automation-account"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Basic"
 
   identity {
